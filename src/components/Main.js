790b48803970190a000d967a5674651683d91a61
kiboma <omayioben@gmail.com>
2023-12-08 18:46:23 +0300

    Display list and add filter buttons

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,8 +1,39 @@
-import React from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 
 const Main = () => {
+
+    const [books, setBooks] = useState([]);
+    const [url,setUrl] = useState("http://localhost:8000/books");
+
+    console.log(books);
+
+    const fetchUrl = useCallback(
+        async() => {
+            const response = await fetch(url)
+            const data = await response.json();
+            setBooks(data);
+        },[url]);
+
+    useEffect(() => {
+        fetchUrl();
+    },[fetchUrl])
+
+
   return (
     <div className='main'>
+        <div className='filter-btns'>
+            <button onClick={()=>setUrl("http://localhost:8000/books")}>All Books</button>
+            <button onClick={()=>setUrl("http://localhost:8000/books?best_author=true")}>Best Author</button>
+        </div>
+        {books.map(book =>(
+            <div key={book.id} className='card'>
+                <h2>{book.book_name}</h2>
+                <p>{book.description} </p>
+                <p>Published in the year {book.year_published }</p>
+                <h3>{book.best_author?"*****":"**"}</h3>
+            </div>
+        ))}
+
       
     </div>
   )
